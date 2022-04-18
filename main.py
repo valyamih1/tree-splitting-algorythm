2c52d17a7ad9bfaec4951bfa2f1357860b748238
val <valyamih1@gmail.com>
2022-04-19 01:41:57 +0300

    version 2

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import graphics as graph
 import systems as sys
 import generators as gen
+# import random
+# import matplotlib.pyplot as plt
+#
+# N = 500
+# x = range(N)
+# y = [random.randint(0, 18) for i in x]
+# plt.plot(x, y, 'o')
+# plt.show()
+# plt.hist(y, 1)
+# plt.show()
 
-
-K = 5000  # число пользователей
+K = 50  # число пользователей
 # time = 10000  # количество окон
 # L = 0.375
-alpha = 0.5
-q_ary = 18
-preamb = 54
+# alpha = 0.5
+q_ary = 4
+preamb = 8
 
 sys.tree_splitting(K, q_ary, preamb)
 # graph.graphics_tree_splitting(K, time)
--- systems.py
+++ systems.py
@@ -2,53 +2,72 @@ import random
 import generators as gen
 import numpy as np
 import math
+import statistics
+import pprint
 
 
-def binary_tree(time, rao, preamb, q, level):
+def binary_tree(resolved_users, rao, preamb, q, level, outage):
     G = int(len(preamb) / q)
     trao = []
     num_of_used_preamb = 0
     is_resolved = False
-    if level == 0:
-        for i in range(len(rao)):
-            if len(rao[i]) != 0:
-                trao.append([])
-                for q_1 in range(q):
-                    trao[i].append([])
-            for j in range(len(rao[i])):
-                num_of_used_preamb += 1
-                if num_of_used_preamb <= G*q:
-                    rao[i][j][2] += 1
-                else:
-                    rao[i][j][2] += 2
-                rao[i][j][3] += 1
-                p = random.randint(0, q - 1)
-                trao[i][p].append((rao[i][j]))
-    else:
-        for i in range(len(rao)):
+    new_rao =[]
+    # if level == 0:
+    for i in range(len(rao)):
+        if len(rao[i]) != 0:
             trao.append([])
-            for j in range(len(rao[i])):
-                if len(rao[i][j]) > 1:
-                    for q_1 in range(q):
-                        trao[i].append([])
-                    for k in range(len(rao[i][j])):
-                        num_of_used_preamb += 1
-                        if num_of_used_preamb <= G*q:
-                            rao[i][j][k][2] += 1
-                        else:
-                            rao[i][j][k][2] += num_of_used_preamb % (G*q)
-                        rao[i][j][k][3] += 1
-                        p = random.randint(0, q - 1)
-                        trao[i][p].append((rao[i][j][k]))
+            for q_1 in range(q):
+                trao[i].append([])
+        for j in range(len(rao[i])):
+            num_of_used_preamb += 1
+            if num_of_used_preamb <= G*q:
+                rao[i][j][2] += 1
+            else:
+                rao[i][j][2] += 1 + num_of_used_preamb % (G*q)
+            rao[i][j][3] += 1
+            p = int(10*random.uniform(0, (q/10)))
+            trao[i][p].append((rao[i][j]))
+# else:
+    #     for i in range(len(rao)):
+    #         trao.append([])
+    #         for j in range(len(rao[i])):
+    #             if len(rao[i][j]) > 1:
+    #                 for q_1 in range(q):
+    #                     trao[i].append([])
+    #                 for k in range(len(rao[i][j])):
+    #                     if rao[i][j][k][3] > 10:
+    #                         outage.append(rao[i][j][k])
+    #                         # rao[i][j].pop(k)
+    #                         break
+    #                     rao[i][j][k][3] += 1
+    #                     num_of_used_preamb += 1
+    #                     if num_of_used_preamb <= G*q:
+    #                         rao[i][j][k][2] += 1
+    #                     else:
+    #                         rao[i][j][k][2] += 1 + num_of_used_preamb % (G*q)
+    #                     p = int(10*random.uniform(0, (q/10)))
+    #                     trao[i][p].append((rao[i][j][k]))
+    num_of_levels = 0
     for i in range(len(trao)):
         for j in range(len(trao[i])):
-            if len(trao[i][j]) > 1:
-                is_resolved = False
-                binary_tree(time, trao, preamb, q, level+1)
-            else:
+            if len(trao[i][j]) == 1:
                 is_resolved = True
+                resolved_users.append(trao[i][j])
+            elif len(trao[i][j]) > 1:
+                num_of_levels += 1
+                is_resolved = False
+                new_rao.append(trao[i][j])
     if is_resolved:
-        return trao
+        pprint.pprint(trao, width=30)
+        return trao, outage, resolved_users
+    else:
+        if (math.ceil(num_of_levels / (G*q))) > 1:
+            for i in range(math.ceil(num_of_levels / (G*q))):
+                trao, outage, resolved_users = binary_tree(resolved_users, new_rao[0:(G*q)], preamb, q, level + 1, outage)
+                del new_rao[0:(G*q)]
+        else:
+            trao, outage, resolved_users = binary_tree(resolved_users, new_rao, preamb, q, level + 1, outage)
+        return trao, outage, resolved_users
 
 
 def tree_splitting(K, q, m):
@@ -59,6 +78,11 @@ def tree_splitting(K, q, m):
     num_pr_transmissions = 0
     rao = []
     trao = []
+    outage = []
+    resolved_users = []
+    # r = []
+    # for i in range(K):
+    #     r.append(int(10*random.uniform(0, (q/10))))
     for i in range(m):
         preamb.append(0)
         rao.append([])
@@ -74,8 +98,15 @@ def tree_splitting(K, q, m):
             for j in range(len(user_buffers)):
                 if user_buffers[j][1] == i:
                     rao[pr_counter].append(user_buffers[j])
-    trao = binary_tree(time, rao, preamb, q, 0)
-    for i in range(len(trao)):
-        for j in range(len(trao[i])):
-            if len(trao[i][j]) != 0:
-                print(trao[i][j])
+        elif preamb[i] == 1:
+            for j in range(len(user_buffers)):
+                if user_buffers[j][1] == i:
+                    resolved_users.append(user_buffers[j])
+    trao, outage, resolved_users = binary_tree(resolved_users, rao, preamb, q, 0, outage)
+    print(resolved_users)
+    print(len(resolved_users))
+
+    # for i in range(len(trao)):
+    #     for j in range(len(trao[i])):
+    #         if len(trao[i][j]) != 0:
+    #             print(trao[i][j])
