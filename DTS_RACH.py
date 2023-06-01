48a09d8e4362ad48bb60aaabbf31fe66f10aa1c4
val <valyamih1@gmail.com>
2023-06-02 02:21:53 +0300

    Add standard random access procedure; delays, success rates, transmissions are looking good, only problem is throughput

--- DTS_RACH.py
+++ DTS_RACH.py
@@ -35,7 +35,18 @@ def get_distribution(time_of_burst_arrivals, N):
 
 
 def first_access_attempt(time_of_burst_arrivals, N, max_rao, total_preambles_in_system):
-    distibution_n_i, new_N = get_distribution(time_of_burst_arrivals, N)
+    distribution_n_i, new_N = get_distribution(time_of_burst_arrivals, N)
+    exact_devices = []
+    for i in range(new_N):
+        exact_devices.append((-1,-1))
+    dev_total = 0
+    for index, slot in enumerate(distribution_n_i):
+        # if slot != 0:
+        #
+        for j in range(slot):
+            exact_devices[dev_total] = (j+1, index, 1)
+            dev_total += 1
+
     success_probability = []
     idle_probability = []
     collision_probability = []
@@ -50,41 +61,46 @@ def first_access_attempt(time_of_burst_arrivals, N, max_rao, total_preambles_in_
         collided_preambles.append([])
         collided_requests.append([])
     for i in range(time_of_burst_arrivals):
-        success_probability[i].append((distibution_n_i[i] / total_preambles_in_system) * pow(1 - (1 / total_preambles_in_system), distibution_n_i[i] - 1))
-        idle_probability[i].append(pow(1 - (1 / total_preambles_in_system), distibution_n_i[i]))
+        success_probability[i].append((distribution_n_i[i] / total_preambles_in_system) * pow(1 - (1 / total_preambles_in_system), distribution_n_i[i] - 1))
+        idle_probability[i].append(pow(1 - (1 / total_preambles_in_system), distribution_n_i[i]))
         collision_probability[i].append(1 - success_probability[i][0] - idle_probability[i][0])
         successful_requests[i].append(math.ceil(total_preambles_in_system * success_probability[i][0]))
         # collided_preambles[i].append(((total_preambles_in_system * collision_probability[i][0]), time_of_burst_arrivals))
-        collided_requests[i].append((distibution_n_i[i] - successful_requests[i][0], time_of_burst_arrivals))
+        collided_requests[i].append((distribution_n_i[i] - successful_requests[i][0], time_of_burst_arrivals))
+        for j in range(len(exact_devices)):
+            if exact_devices[j][1] == i:
+                if successful_requests[i][0] < exact_devices[j][0]:
+                    exact_devices[j] =(exact_devices[j][0],exact_devices[j][1], 1 + exact_devices[j][2])
+                else:
+                    exact_devices[j] =(exact_devices[j][0],exact_devices[j][1], 0 - exact_devices[j][2])
+
         if collided_requests[i][0][0] > 0:
             collided_preambles[i].append(
                 (math.ceil(total_preambles_in_system * collision_probability[i][0]), time_of_burst_arrivals))
         else:
             collided_preambles[i].append(
                 (math.floor(total_preambles_in_system * collision_probability[i][0]), time_of_burst_arrivals))
-    return collided_preambles, collided_requests, successful_requests, new_N
+    return collided_preambles, collided_requests, successful_requests, new_N, exact_devices
 
 
-def dynamic_tree_splitting(old_N):
+def dynamic_tree_splitting(old_N, kmax):
     max_rao = 50
-    k_max = 8
-    number_of_collided_preambles = []
+    k_max = kmax - 1
     collision_coefficient = []
-    number_of_collided_request = []
     total_preambles_in_system = 54
     number_of_branches = []
     # N = 50000
     time_of_burst_arrivals = 50
     delay_in_every_subtree = []
-    successful_devices = []
-    number_of_collided_preambles, number_of_collided_request, successful_devices, N = first_access_attempt(time_of_burst_arrivals, old_N, max_rao,total_preambles_in_system)
+    sra_preamb, sra_collisons, sra_success, sra_N, sra_exact_devices = my_sra(1000000, time_of_burst_arrivals, old_N, max_rao, total_preambles_in_system)
+    number_of_collided_preambles, number_of_collided_request, successful_devices, N, exact_devices = first_access_attempt(time_of_burst_arrivals, old_N, max_rao,total_preambles_in_system)
     for i in range(max_rao):
         collision_coefficient.append([])
         number_of_branches.append([])
         if number_of_collided_request[i][0][0] == 0:
             delay_in_every_subtree.append(0)
         else:
-            delay_in_every_subtree.append(12 * time_of_burst_arrivals)
+            delay_in_every_subtree.append(time_of_burst_arrivals)
         for k in range(k_max):
             collision_coefficient[i].append(-1)
             number_of_branches[i].append(0)
@@ -115,14 +131,25 @@ def dynamic_tree_splitting(old_N):
                     number_of_branches[i][k] = 0
 
                 total_preambles_for_each_step = number_of_collided_preambles[i][k][0] * number_of_branches[i][k]
-                delay_in_every_subtree[i] += 12 * (total_preambles_for_each_step/total_preambles_in_system)
+                delay_in_every_subtree[i] += 22 * (total_preambles_for_each_step/total_preambles_in_system)
                 length_of_level_k = math.ceil(total_preambles_for_each_step/total_preambles_in_system)
+                # delay_in_every_subtree[i] += math.ceil(total_preambles_for_each_step/total_preambles_in_system)
                 secondary_success_probability = (number_of_collided_request[i][k][0] / total_preambles_for_each_step) * pow(1 - 1 / total_preambles_for_each_step, number_of_collided_request[i][k][0] - 1)
                 secondary_idle_probability = pow(1 - 1 / total_preambles_for_each_step, number_of_collided_request[i][k][0])
                 secondary_collision_probability = 1 - secondary_success_probability - secondary_idle_probability
                 secondary_successful_devices = math.ceil(total_preambles_for_each_step * secondary_success_probability)
                 secondary_collided_preambles = math.floor(total_preambles_for_each_step * secondary_collision_probability)
                 successful_devices[i].append(secondary_successful_devices)
+                for j in range(len(exact_devices)):
+                    if exact_devices[j][1] == i and exact_devices[j][2] > 0:
+                        step_for_devices = j
+                        break
+                for j in range(len(exact_devices)):
+                    if exact_devices[j][1] == i and exact_devices[j][2] > 0:
+                        if secondary_successful_devices + step_for_devices < j:
+                            exact_devices[j] = (exact_devices[j][0], exact_devices[j][1], 1 + exact_devices[j][2])
+                        else:
+                            exact_devices[j] = (exact_devices[j][0], exact_devices[j][1], 0 - exact_devices[j][2])
                 if secondary_collided_preambles > 0:
                     secondary_collided_devices = number_of_collided_request[i][k][0] - secondary_successful_devices
                 else:
@@ -150,13 +177,106 @@ def dynamic_tree_splitting(old_N):
     total_delay = 0
     # print(successful_devices)
     sum_of_successful_devices = 0
+    transmissions = 0
+
     for i in range(len(number_of_collided_request)):
         outage_devices += number_of_collided_request[i][len(number_of_collided_request[i])-1][0]
         maximal_rao = max(number_of_collided_request[i][0][1], maximal_rao)
         total_delay += delay_in_every_subtree[i]
         sum_of_successful_devices += sum(successful_devices[i])
+    for i in range(len(exact_devices)):
+        if exact_devices[i][2] < 0:
+            transmissions += abs(exact_devices[i][2])
+        if exact_devices[i][2] < -kmax:
+            print(exact_devices[i])
+    if total_delay == 0:
+        total_delay = 50
+    mean_transmissions = transmissions/sum_of_successful_devices
     mean_delay = total_delay/time_of_burst_arrivals
-    mean_throughput = (N - outage_devices)/(total_preambles_in_system*total_delay)
-    success_rate = 1 - (outage_devices / N)
-    print(outage_devices, maximal_rao, success_rate)
-    return mean_throughput, mean_delay, success_rate
+    mean_throughput = 26*(N - outage_devices)/(total_preambles_in_system*total_delay)
+    success_rate = round(1 - (outage_devices / N), 5)
+    sra_outage_devices = 0
+    sra_delay = 0
+    sra_transm = 0
+    sra_counter = 0
+    for i in range(len(sra_exact_devices)):
+        if sra_exact_devices[i][3] == -15:
+            sra_outage_devices += 1
+        else:
+            sra_counter += 1
+            sra_delay += 10 * sra_exact_devices[i][0]
+        sra_transm += abs(sra_exact_devices[i][2])
+    sra_throughput = len(sra_exact_devices)/(total_preambles_in_system * (sra_delay/10))
+    sra_mean_transm = sra_transm/len(sra_exact_devices)
+    sra_success_rate = round(1-(sra_outage_devices/len(sra_exact_devices)), 5)
+    sra_mean_delay = sra_delay/len(sra_exact_devices)
+
+    print(outage_devices,mean_delay, mean_throughput, maximal_rao, success_rate, mean_transmissions, sra_success_rate, sra_mean_delay, sra_mean_transm, sra_throughput)
+    return mean_throughput, mean_delay, success_rate, mean_transmissions, sra_success_rate, sra_mean_delay, sra_mean_transm, sra_throughput
+
+
+def my_sra(max_time, time_of_burst_arrivals, N, max_rao, total_preambles_in_system):
+    distribution_n_i, new_N = get_distribution(time_of_burst_arrivals, N)
+    exact_devices = []
+    for i in range(new_N):
+        exact_devices.append((-1, -1))
+    dev_total = 0
+    for index, slot in enumerate(distribution_n_i):
+        # if slot != 0:
+        #
+        for j in range(slot):
+            exact_devices[dev_total] = (j + 1, index, 1, 0)
+            dev_total += 1
+
+    success_probability = []
+    idle_probability = []
+    collision_probability = []
+    successful_requests = []
+    collided_requests = []
+    collided_preambles = []
+    for i in range(max_time):
+        distribution_n_i.append(0)
+        success_probability.append(0)
+        idle_probability.append(0)
+        collision_probability.append(0)
+        successful_requests.append(0)
+        collided_preambles.append(0)
+        collided_requests.append(0)
+    for i in range(max_time):
+        success_probability[i] =(distribution_n_i[i] / total_preambles_in_system) * pow(1 - (1 / total_preambles_in_system),
+                                                                    distribution_n_i[i] - 1)
+        idle_probability[i] = pow(1 - (1 / total_preambles_in_system), distribution_n_i[i])
+        collision_probability[i] = 1 - success_probability[i] - idle_probability[i]
+        successful_requests[i] = math.ceil(total_preambles_in_system * success_probability[i])
+        # collided_preambles[i].append(((total_preambles_in_system * collision_probability[i][0]), time_of_burst_arrivals))
+        collided_requests[i] = (distribution_n_i[i] - successful_requests[i], time_of_burst_arrivals)
+        samples = random.sample(range(1, distribution_n_i[i]+1),successful_requests[i])
+        counter = 0
+        for j in range(len(exact_devices)):
+            if exact_devices[j][1] == i and exact_devices[j][3] >= 0:
+                counter += 1
+                if exact_devices[j][0] in samples:
+                    exact_devices[j] = (exact_devices[j][0], exact_devices[j][1], 0 - exact_devices[j][2], -1)
+                else:
+                    backoff = random.randint(1, 4)
+                    if exact_devices[j][2]+1 > 8:
+                        exact_devices[j] = (exact_devices[j][0], exact_devices[j][1], exact_devices[j][2], -15)
+                    else:
+                        exact_devices[j] = (distribution_n_i[i + backoff] + 1, exact_devices[j][1] + backoff, 1 + exact_devices[j][2], backoff)
+                        distribution_n_i[i + backoff] += 1
+            if counter >= distribution_n_i[i]:
+                break
+                # if successful_requests[i] < exact_devices[j][0]:
+                #     backoff = random.randint(1,4)
+                #     distribution_n_i[i+backoff] += 1
+                #     exact_devices[j] = (exact_devices[j][0], exact_devices[j][1], 1 + exact_devices[j][2], random.randint(1,4))
+                #
+                # else:
+                #     exact_devices[j] = (exact_devices[j][0], exact_devices[j][1], 0 - exact_devices[j][2])
+
+        if collided_requests[i][0] > 0:
+            collided_preambles[i] = (math.ceil(total_preambles_in_system * collision_probability[i]), time_of_burst_arrivals)
+        else:
+            collided_preambles[i] = (math.floor(total_preambles_in_system * collision_probability[i]), time_of_burst_arrivals)
+    return collided_preambles, collided_requests, successful_requests, new_N, exact_devices
+
--- main.py
+++ main.py
@@ -11,17 +11,16 @@ def tree_rach_graphs():
     sys.setrecursionlimit(1000000)
     # K = [1000, 5000, 10000, 20000, 30000]  # число пользователей
     # K = [1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56, 61, 66, 71, 76, 81, 86, 91, 96, 101]
-    K = np.arange(1, 20007, 50)
-    q_ary = 18
-        # , 9, 18]
+    K = np.arange(5000, 50001, 5000)
+    q_ary = [6, 9, 18]
     preamb = 54
     delay = []
-    with open('delay with 1 to 20000 devices, step 50.txt') as f:
-        lines = f.read()
-    delay = lines.split(', ')
-    delays = []
-    for i in delay:
-        delays.append(float(i))
+    # with open('delay with 1 to 20000 devices, step 50.txt') as f:
+    #     lines = f.read()
+    # delay = lines.split(', ')
+    # delays = []
+    # for i in delay:
+    #     delays.append(float(i))
     # K = []
     # for i in range(1, 2502, 5):
     #     K.append(i)
@@ -32,7 +31,7 @@ def tree_rach_graphs():
     #     for j in range(500):
     #         mean_delay += tree_RACH.tree_splitting(i, q_ary, preamb)
     #     delay.append(48*mean_delay/500)
-    # tree_RACH.tree_splitting(K, q_ary, preamb)
+    tree_RACH.tree_splitting(K, q_ary, preamb)
     ### hardcoded values from working script in PyPy, just to make plots in CPython ###
     transm_6 = [3.417, 4.3392, 4.7341, 5.1317, 5.3583]
     transm_9 = [3.112, 3.8168, 4.1402, 4.4556, 4.6156]
@@ -44,7 +43,7 @@ def tree_rach_graphs():
     plt.figure(1)
 
     # plt.plot(K, transm_rao, label='10 RAO', marker=("."), linewidth=2, markersize=7)
-    plt.plot(K, delays, label='TRAO, q = 6', marker=".", linewidth=2, markersize=7, linestyle='-.')
+    # plt.plot(K, delays, label='TRAO, q = 6', marker=".", linewidth=2, markersize=7, linestyle='-.')
     # plt.plot(K, total_trao_9, label='TRAO, q = 9', marker=".", linewidth=2, markersize=7, linestyle=':')
     # plt.plot(K, total_trao_18, label='TRAO, q = 18', marker=".", linewidth=2, markersize=7, linestyle='--')
     plt.xlabel('Number of devices')
@@ -54,18 +53,58 @@ def tree_rach_graphs():
     plt.show()
 
 
-def dts_rach_graphs():
+def dts_rach_graphs(kmax):
     K = np.arange(5000, 50007, 5000)
     throughputs = []
     delays = []
     rates = []
+    transms = []
+    sra_rates = []
+    sra_delays = []
+    sra_transm = []
+    sra_thrs = []
+    # throughputs_100 = []
+    # delays_100 = []
+    # rates_100 = []
+    # transms_100 = []
+    # sra_rates_100 = []
+    # sra_delays_100 = []
+    # sra_transm_100 = []
+    # m = 50
+    # for j in range(len(K)):
+    #     throughputs_100.append(0)
+    #     delays_100.append(0)
+    #     rates_100.append(0)
+    #     transms_100.append(0)
+    #     sra_rates_100.append(0)
+    #     sra_delays_100.append(0)
+    #     sra_transm_100.append(0)
+    # for j in range(m):
+
     for i in K:
-        th, delay, rate = DTS_RACH.dynamic_tree_splitting(i)
+        th, delay, rate, transm, sra_rate, sra_delay, sra_tr, sra_thr = DTS_RACH.dynamic_tree_splitting(i,kmax)
         throughputs.append(th)
         delays.append(delay)
         rates.append(rate)
+        transms.append(transm)
+        sra_rates.append(sra_rate)
+        sra_delays.append(sra_delay)
+        sra_transm.append(sra_tr)
+        sra_thrs.append(sra_thr)
+        # for i in range(len(K)):
+        #     throughputs_100[i] += throughputs[i] / m
+        #     delays_100[i] += delays[i] / m
+        #     rates_100[i] += rates[i] / m
+        #     transms_100[i] += transms[i] / m
+        #     sra_delays_100[i] += sra_delays[i] / m
+        #     sra_transm_100[i] += sra_transm[i] / m
+        #     sra_rates_100[i] += sra_rates[i] / m
+
     plt.figure(1)
-    plt.plot(K, throughputs, label='Mean Throughput', marker=".", linewidth=2, markersize=7, linestyle='--')
+    # plt.ylim(0, 0.4)
+    # plt.xlim(5000, 50000)
+    plt.plot(K, throughputs, label='DTS Mean Throughput', marker=".", linewidth=2, markersize=7)
+    plt.plot(K, sra_thrs, label='SRA Mean Throughput', marker=".", linewidth=2, markersize=7, linestyle='--')
     plt.xlabel('Number of devices')
     plt.ylabel('Mean Throughput')
     plt.grid(True)
@@ -73,7 +112,10 @@ def dts_rach_graphs():
     plt.show()
 
     plt.figure(2)
-    plt.plot(K, delays, label='Mean delay', marker=".", linewidth=2, markersize=7, linestyle='--')
+    # plt.ylim(0, 1600)
+    # plt.xlim(5000, 50000)
+    plt.plot(K, delays, label='DTS Mean delay', marker=".", linewidth=2, markersize=7)
+    plt.plot(K, sra_delays, label='SRA Mean delay', marker=".", linewidth=2, markersize=7, linestyle='--')
     plt.xlabel('Number of devices')
     plt.ylabel('Mean delay')
     plt.grid(True)
@@ -81,15 +123,29 @@ def dts_rach_graphs():
     plt.show()
 
     plt.figure(3)
-    plt.plot(K, rates, label='Success rate', marker=".", linewidth=2, markersize=7, linestyle='--')
+    # plt.ylim(0, 1)
+    # plt.xlim(5000, 50000)
+    plt.plot(K, rates, label='DTS Success rate', marker=".", linewidth=2, markersize=7)
+    plt.plot(K, sra_rates, label='SRA Success rate', marker=".", linewidth=2, markersize=7, linestyle='--')
     plt.xlabel('Number of devices')
     plt.ylabel('Success rate')
     plt.grid(True)
     plt.legend()
     plt.show()
 
+    plt.figure(4)
+    # plt.ylim(0, 10)
+    # plt.xlim(5000, 50000)
+    plt.plot(K, transms, label='DTS Mean number of transmissions', marker=".", linewidth=2, markersize=7)
+    plt.plot(K, sra_transm, label='SRA Mean number of transmissions', marker=".", linewidth=2, markersize=7, linestyle='--')
+    plt.xlabel('Number of devices')
+    plt.ylabel('Transmissions')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
 
 if __name__ == "__main__":
-    # dts_rach_graphs()
-    th, delay, rate = DTS_RACH.dynamic_tree_splitting(50000)
+    dts_rach_graphs(8)
+    # th, delay, rate, transm = DTS_RACH.dynamic_tree_splitting(50000, 8)
+
     # sra.graphs()
